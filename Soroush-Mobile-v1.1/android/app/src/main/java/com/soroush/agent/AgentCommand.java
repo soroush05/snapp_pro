9cbb33868e5348e9fa87db55ee796336f6b49fa1
soroush <80487137+soroush05@users.noreply.github.com>
2026-09-23 12:38:25 -0700

    Add files via upload

--- Soroush-Mobile-v1.1/android/app/src/main/java/com/soroush/agent/AgentCommand.java
+++ Soroush-Mobile-v1.1/android/app/src/main/java/com/soroush/agent/AgentCommand.java
@@ -5,7 +5,17 @@ public final class AgentCommand {
     public final Type type;
     public final String origin;
     public final String destination;
+    public final double destinationLat;
+    public final double destinationLon;
+
     public AgentCommand(Type type, String origin, String destination) {
-        this.type=type; this.origin=origin; this.destination=destination;
+        this(type, origin, destination, Double.NaN, Double.NaN);
     }
+
+    public AgentCommand(Type type, String origin, String destination, double destinationLat, double destinationLon) {
+        this.type=type; this.origin=origin; this.destination=destination;
+        this.destinationLat=destinationLat; this.destinationLon=destinationLon;
+    }
+
+    public boolean hasDestinationPoint(){ return !Double.isNaN(destinationLat) && !Double.isNaN(destinationLon); }
 }
